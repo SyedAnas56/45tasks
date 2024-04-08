74bd1f2842d734b768492eaa03a033e92f32a661
Syed Anas <163395358+SyedAnas56@users.noreply.github.com>
2024-04-08 05:54:26 -0700

    Add files via upload
    ....

---
+++ TASK1-4.ts
@@ -0,0 +1,26 @@
+
+/*Task2
+var a="hello world"
+console.log(a);
+
+
+
+
+/*Task3
+let a: string=("Muhammad Anas")
+let lowercase: string=a.toLowerCase();
+console.log(lowercase);
+let uppercase: string=a.toUpperCase();
+console.log(uppercase);
+let word=a.slice(0,1);
+let Titlecase= word.toLocaleUpperCase();
+
+console.log(Titlecase+a.slice(1));/*
+
+/*Task4
+console.log('imran khan once said,"we shall never lie and always speak the truth"')
+
+/*Task 5
+let famous_person="imran khan"
+let message:string =famous_person + ' once said,"we shall never lie and always speak the truth"'
+console.log(message)
